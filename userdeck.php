c8953d645002938e1c38b2565eedf508519c6f06
Derek Myers <arcticpro@gmail.com>
2014-02-13 01:54:38 -0600

    Initial commit

---
+++ userdeck.php
@@ -0,0 +1,245 @@
+<?php
+/**
+ * Plugin Name: UserDeck
+ * Plugin URI: http://userdeck.com/plugins/wordpress
+ * Description: WordPress plugin for <a href="http://userdeck.com">UserDeck</a>.
+ * Version: 0.0.1
+ * Author: UserDeck
+ * Author URI: http://userdeck.com
+ */
+
+defined( 'ABSPATH' ) or die();
+
+class UserDeck {
+	
+	private static $instance = null;
+
+	public static function get_instance() {
+
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+
+	}
+	
+	/**
+	 * class constructor
+	 * register the activation and de-activation hooks and hook into a bunch of actions
+	 */
+	public function __construct() {
+		
+		add_action( 'wp_footer', array( $this, 'output_feedback_code' ) );
+		add_action( 'admin_menu', array( $this, 'create_options_page') );
+		add_action( 'admin_init', array( $this, 'settings_init') );
+		add_action( 'admin_notices', array( $this, 'notice') );
+		add_shortcode( 'userdeck_kb', array( $this, 'output_knowledgebase_code') );
+		
+	}
+	
+	/**
+	 * retrieve the userdeck options
+	 * @return array 'userdeck' options
+	 */
+	public function get_settings() {
+		
+		return get_option( 'userdeck', array('helpdesk_id' => null) );
+		
+	}
+
+	/**
+	 * update the userdeck options in the database
+	 * @param  array $options new options settings to save
+	 * @return null
+	 */
+	function update_settings( $options ) {
+
+		update_option( 'userdeck', $options );
+
+	}
+	
+	/**
+	 * output the userdeck feedback javascript install code
+	 * @return null
+	 */
+	public function output_feedback_code() {
+		
+		// retrieve the options
+		$options = self::get_settings();
+		
+		$helpdesk_id = $options['helpdesk_id'];
+		
+		$out = '<script type="text/javascript">';
+		$out .= '	(function() {';
+		$out .= '		var f = document.createElement(\'script\'); f.type = \'text/javascript\'; f.async = true; f.id = \'feedbackapp\';';
+		$out .= '		f.src = \'http://userdeck.com/assets/bundles/helpdesk/feedback.js?id=' . $helpdesk_id . '\';';
+		$out .= '		var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(f, s);';
+		$out .= '	})();';
+		$out .= '</script>';
+		
+		echo $out;
+		
+	}
+	
+	/**
+	 * output the userdeck knowledge base javascript install code
+	 * @return null
+	 */
+	public function output_knowledgebase_code() {
+		
+		// retrieve the options
+		$options = self::get_settings();
+		
+		$helpdesk_id = $options['helpdesk_id'];
+		
+		$out = '<div id="kbapp"></div>';
+		$out .= '<script type="text/javascript">';
+		$out .= '	var KBAppOptions = {id : "' . $helpdesk_id . '"};';
+		$out .= '	document.write(\'\x3Cscript type="text/javascript" src="\'';
+		$out .= '	+ \'http://userdeck.com/assets/bundles/helpdesk/kb.js\'';
+		$out .= '	+ \'">\x3C/script>\');';
+		$out .= '</script>';
+		
+		echo $out;
+		
+	}
+	
+	/**
+	 * show a 'settings saved' notice
+	 * and a friendly reminder if the app ID or secret key haven't been entered
+	 * @return null
+	 */
+	function notice() {
+
+		if ( isset( $_GET['page'] ) and ( 'userdeck' == $_GET['page'] ) ) {
+
+			if ( isset( $_GET['updated'] ) ) { ?>
+				<div class="updated" id="userdeck-updated"><p><strong><?php _e( 'Settings saved.' ); ?></strong></p></div>
+				<?php
+			}
+
+		}
+
+		// show a reminder to users who can update options
+
+		if ( ! current_user_can( 'manage_options' ) )
+			return;
+
+		$options = self::get_settings();
+
+		if ( !isset( $options['helpdesk_id'] ) or !$options['helpdesk_id'] ) {
+			echo '<div class="error" id="userdeck-notice"><p><strong>UserDeck needs some attention</strong>. ';
+			if ( isset( $_GET['page'] ) and 'userdeck' == $_GET['page'] ) {
+				echo 'Please enter your UserDeck helpdesk ID';
+			} else {
+				echo 'Please <a href="options-general.php?page=userdeck">configure the UserDeck settings</a>';
+			}
+			echo ' to start tracking your users.</p></div>' . "\n";
+		}
+
+	}
+	
+	/**
+	 * create the relevant type of options page
+	 * @return null
+	 */
+	public function create_options_page() {
+		
+		add_options_page('UserDeck Settings', 'UserDeck', 'manage_options', 'userdeck', array($this, 'render_options_page'));
+		
+	}
+	
+	/**
+	 * output the options page
+	 * @return null
+	 */
+	public function render_options_page() {
+		
+		$options = self::get_settings();
+		
+		$action = 'options.php';
+		
+		?>
+		
+		<div class="wrap">
+		
+			<?php screen_icon( 'options-general' ); ?>
+			<h2>UserDeck Configuration</h2>
+			
+			<div class="postbox-container" style="width:65%;">
+
+				<form method="post" action="<?php echo $action; ?>">
+
+					<?php settings_fields( 'userdeck' ); ?>
+
+					<table class="form-table">
+						<tbody>
+
+							<tr valign="top">
+								<th scope="row">HelpDesk ID</th>
+								<td>
+									<input name="userdeck[helpdesk_id]" type="text" value="<?php echo esc_attr( $options['helpdesk_id'] ); ?>">
+								</td>
+							</tr>
+
+						</tbody>
+						
+					</table>
+					
+					<p class="submit">
+						<input class="button-primary" name="userdeck-submit" type="submit" value="Save Settings">
+					</p>
+					
+				</form>
+				
+			</div>
+			
+		</div>
+		
+		<?php
+		
+	}
+	
+	/**
+	 * use the WordPress settings api to initiate the various settings
+	 * and if it's a network settings page then validate & update any submitted settings
+	 * @return null
+	 */
+	public function settings_init() {
+		
+		register_setting( 'userdeck', 'userdeck', array( $this, 'validate' ) );
+		if ( isset( $_REQUEST['_wpnonce'] ) and wp_verify_nonce( $_REQUEST['_wpnonce'], 'userdeck-options' ) ) {
+
+			$file = 'options-general.php';
+
+			if ( isset( $_POST['userdeck-submit'] ) ) {
+				$options = self::validate( $_POST['userdeck'] );
+				self::update_settings( $options );
+				wp_redirect( add_query_arg( array(
+					'page'    => 'userdeck',
+					'updated' => true
+					), $file ) );
+				die();
+			}
+
+		}
+		
+	}
+	
+	/**
+	 * make sure that no dodgy stuff is trying to sneak through
+	 * @param  array $input options to validate
+	 * @return array        validated options
+	 */
+	function validate( $input ) {
+
+		$new['helpdesk_id'] = wp_kses( trim( $input['helpdesk_id'] ), array() );
+
+		return $new;
+
+	}
+	
+} // class
+
+UserDeck::get_instance();
